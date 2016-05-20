ce4ece17d62725a081e35940ed19d8a9c1f4f2e7
Jánossy Bálint <Jnossy Blint>
2016-05-20 13:03:41 +0200

    Új struct a táblához, hogy kezelni lehessen, hogy melyik a nyertes sor/oszlop.

---
+++ Amobakocka.cpp
@@ -0,0 +1,8 @@
+#include "Amobakocka.hpp"
+
+Amoba_kocka::Amoba_kocka(char ertek,bool nyert){
+    _ertek=ertek;
+    _nyert=nyert;
+}
+//Amoba_kocka::Amoba_kocka(){}
+
---
+++ Amobakocka.hpp
@@ -0,0 +1,15 @@
+#ifndef AMOBAKOCKA_HPP
+#define AMOBAKOCKA_HPP
+
+#include "Amobakocka.hpp"
+
+struct Amoba_kocka{
+    char _ertek;
+    bool _nyert;
+    Amoba_kocka(char ertek,bool nyert); //string allapot
+   // Amoba_kocka();
+};
+
+#endif // AMOBAKOCKA_HPP
+
+
